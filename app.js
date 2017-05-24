530b058dc3bf5a33cabba79da9e12588366b3603
yusukemisa <yusukemisawa@misawayuusuke-no-MacBook-Air.local>
2017-05-24 23:06:26 +0900

    no message

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const restify = require('restify');
 const request = require('request');
 const builder = require('botbuilder');
 
+// ローカルの場合.envからアプリ設定値を取得（Azureのダッシュボード上の設定＞アプリケーション設定＞アプリ設定値でやってることと同じ）
+if (!process.env.SLACK_WEBFOOK_URL) {
+  require('dotenv').config();
+}
+console.log(process.env);
 // Setup Restify Server
 const server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
@@ -17,19 +22,24 @@ const connector = new builder.ChatConnector({
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
-const options = {
-  uri:　process.env.SLACK_WEBFOOK_URL,
-  headers: {
-    "Content-type": "application/json",
-  },
-  json: {
-    "text":"我々はかしこいのでこのようなこともできるのです。ふんす！:triumph:"
-  }
-};
+function getOptions(message,analized) {
+  let options = {
+    uri:　process.env.SLACK_WEBFOOK_URL,
+    headers: {
+      "Content-type": "application/json",
+    },
+    json: {
+      "text":"お前が言った言葉\n> " + message + "\nについて我々はかしこいので知っているのです。ふんす！:triumph: \n教えて欲しければカレー:curry:を持ってくるのです。\n",
+      "link_names":true,
+      channel:"#github"
+    }
+  };
+  return options;
+}
 
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 const bot = new builder.UniversalBot(connector, function (session) {
-    request.post(options, function(error, response, body){});
+    request.post(getOptions(session.message.text), function(error, response, body){});
     session.send("You said: %s", session.message.text);
 });
